d2dd1a0b7c9c2a3eb4c35be08c9b1448d39b67dd
andrezinrc <andrem.33333@gmail.com>
2024-01-21 10:08:28 -0300

    Adiciona funcionalidades de editar e deletar tarefas

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,17 @@
-from flask import Flask, render_template, redirect, request, flash
+from flask import Flask, render_template, redirect, flash, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from .form import Formulario
+import logging
 
 app = Flask(__name__)
 conexao = "sqlite:///tarefas.db"
 
 app.config["SECRET_KEY"] = "amo-programar"
 app.config["SQLALCHEMY_DATABASE_URI"] = conexao
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app) 
-
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 class Tarefas(db.Model):
@@ -22,13 +23,38 @@ class Tarefas(db.Model):
 def index():
     form = Formulario()
 
-    if form.validate_on_submit():
-        flash('Tarefa adicionada com sucesso!', 'success')
-      
-    if request.method == "POST":
-        tarefa = Tarefas(nome=request.form["nome"], hora=request.form["hora"])
-        db.session.add(tarefa)
-        db.session.commit()
-        return render_template("index.html", form=form, tarefa=tarefa)
+    if request.method == "POST" and form.validate_on_submit():
+        try:
+            tarefa = Tarefas(nome=form.nome.data, hora=form.hora.data)
+            db.session.add(tarefa)
+            db.session.commit()
+            flash('Tarefa adicionada com sucesso!', 'success')
+        except Exception as e:
+            flash(f'Erro ao adicionar tarefa: {e}', 'danger')
+            logging.exception("Erro ao adicionar tarefa")
 
-    return render_template("index.html", form=form)
+    tarefas = Tarefas.query.all()
+    logging.debug("Tarefas no banco de dados: %s", tarefas)
+
+    return render_template("index.html", form=form, tarefas=tarefas)
+
+@app.route("/editar_tarefa/<int:tarefa_id>", methods=["GET", "POST"])
+def editar_tarefa(tarefa_id):
+    tarefa = Tarefas.query.get_or_404(tarefa_id)
+    form = Formulario(obj=tarefa)
+
+    if form.validate_on_submit():
+        form.populate_obj(tarefa)
+        db.session.commit()
+        flash('Tarefa editada com sucesso!', 'success')
+        return redirect(url_for("index"))
+
+    return render_template("editar_tarefa.html", form=form, tarefa=tarefa)
+
+@app.route("/deletar_tarefa/<int:tarefa_id>", methods=["POST"])
+def deletar_tarefa(tarefa_id):
+    tarefa = Tarefas.query.get_or_404(tarefa_id)
+    db.session.delete(tarefa)
+    db.session.commit()
+    flash('Tarefa deletada com sucesso!', 'success')
+    return redirect(url_for("index"))
